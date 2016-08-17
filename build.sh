0b975a26e3ae49750ac9946a7a642c50723f83f6
Primož Sečnik Kolman <primoz@thousandeyes.com>
2016-08-17 18:15:23 +0200

    Moving binaries

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export GOPATH=$PWD
-env GOOS=linux GOARCH=386 go build -o bin/te-ips-linux te-ips
-#env GOOS=linux GOARCH=amd64 go build -o bin/te-ips-linux-amd64 te-ips
-#env GOOS=linux GOARCH=arm go build -o bin/te-ips-linux-arm te-ips
-env GOOS=darwin GOARCH=amd64 go build -o bin/te-ips-macos te-ips
-env GOOS=windows GOARCH=386 go build -o bin/te-ips.exe te-ips
+env GOOS=linux GOARCH=386 go build -o bin/linux-32/te-ips te-ips
+env GOOS=linux GOARCH=amd64 go build -o bin/linux-64/te-ips te-ips
+env GOOS=linux GOARCH=arm go build -o bin/linux-arm/te-ips te-ips
+env GOOS=darwin GOARCH=amd64 go build -o bin/macos/te-ips te-ips
+env GOOS=windows GOARCH=386 go build -o bin/win/te-ips.exe te-ips
